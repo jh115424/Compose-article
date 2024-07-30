3296e408d38ab597daeb8f996eb2d307951dbf78
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-07-29 20:08:55 -0500

    got banner up, need to modify

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,7 +14,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(
+                    ComposeText(
                         name = "Android",
+
 
                         )
 
@@ -36,18 +42,39 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
+    fun ComposeText(name: String, modifier: Modifier = Modifier) {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Box(modifier) {
+         Image(
+             painter = image,
+             contentDescription = null,
+             modifier = modifier
+                 .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
+         )
+        }
         Text(
             text = "Hello $name!",
+            fontSize = 24.sp,
             modifier = modifier
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
         )
     }
+
+    @Composable
+    fun BodyText(name: String, modifier: Modifier = Modifier ) {
+
+
+    }
+
+
+
+
 
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
         ComposeArticleTheme {
-            Greeting("Android")
+            ComposeText("Android")
         }
     }
 }
